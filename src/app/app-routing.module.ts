186f55ac8791bf50bf1ab69458abb703136932ce
Edson Sales <edsonsales99@gmail.com>
2019-09-28 01:25:54 -0500

    fotos adicionadas para a tela feed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-
   { path: 'login', loadChildren: './auth/auth.module#AuthModule' },
   { path: 'usuario', loadChildren: './page/roteamento-paginas.module#RoteamentoPaginasModule',
    canLoad: [AuthGuard]// com isso a pagina nao sera baixada
   }  
   
-  
-  
-
 ];
 
 @NgModule({
