955adfba6ccd44d0ef84f695d8acfd2031e43c09
bruno bortoli <bruno.bortoli25@gmail.com>
2019-11-05 00:01:44 -0500

    Teste 1
    Mal sucedido

--- src/app/page/pages/usuario/usuario.page.ts
+++ src/app/page/pages/usuario/usuario.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService, TypeUser } from 'src/app/service/user.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-usuario',
@@ -7,13 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioPage implements OnInit {
 
+  idea: TypeUser = {
+    nome: '',
+    email: '',
+    tipo : 'Usuario'
+  };
+
   public titulo :string = 'Mands'
   public profissional : boolean =  true // responsavel por definir ser o usuario e profisional ou nao nas regras de template
 
-  constructor() { } 
+
+  constructor(private userService: UserService, private toastCtrl: ToastController) { } 
 
   ngOnInit() {       
-  }        
+  }
+  
+  addIdea() {
+    this.userService.addUser(this.idea).then(() => {
+      this.showToast('Idea added');
+    }, err => {
+      this.showToast('There was a problem adding your idea :(');
+    });
+  }
+
+  showToast(msg) {
+    this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    }).then(toast => toast.present());
+  }
                          
 }                     
      
