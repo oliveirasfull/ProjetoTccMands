072e760c23f03f3a0c00be6a9622c1c6d9a0b866
bruno bortoli <bruno.bortoli25@gmail.com>
2019-11-18 16:22:09 -0500

    Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Push, PushOptions, PushObject } from '@ionic-native/push/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,7 +15,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private push: Push
   ) {
     this.initializeApp();
   }
@@ -23,5 +26,27 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  pushSetup(){
+    const options: PushOptions = {
+      android: {
+        senderID: 524654132
+      },
+      ios: {
+          alert: 'true',
+          badge: true,
+          sound: 'false'
+      }
+   }
+   
+   const pushObject: PushObject = this.push.init(options);
+   
+   
+   pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+   
+   pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+   
+   pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
   }
 }
