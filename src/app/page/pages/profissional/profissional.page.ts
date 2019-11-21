de71d1e0bdb7c88c4f9001bd4ff231d5e0265263
bruno bortoli <bruno.bortoli25@gmail.com>
2019-11-21 09:40:55 -0500

    Calendar

--- src/app/page/pages/profissional/profissional.page.ts
+++ src/app/page/pages/profissional/profissional.page.ts
@@ -51,8 +51,6 @@ export class ProfissionalPage implements OnInit {
         }
       }
     });
-
-    this.resetEvent();
   }
 
 
@@ -121,78 +119,7 @@ export class ProfissionalPage implements OnInit {
 
   // ------------------------- CAlendar ---------------------------
 
-  resetEvent(){
-    this.event = {
-      title: '',
-      desc:'',
-      startTime: new Date().toISOString(),
-      endTime: new Date().toISOString(),
-      allDay: false
-    };
-  }
-
-  addEvent(){
-    let eventCopy = {
-      title: this.event.title,
-      startTime: new Date(this.event.startTime),
-      endTime: new Date(this.event.endTime),
-      allDay: this.event.allDay,
-      desc: this.event.desc
-    }
-
-    if(eventCopy.allDay){
-      let start = eventCopy.startTime;
-
-      eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()))
-      eventCopy.endTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate() + 1))
-    }
-
-    this.eventSource.push(eventCopy);
-    this.myCal.loadEvents();
-    this.resetEvent();
-  }
-
-  changeMode(mode){
-    this.calendar.mode = mode;
-  }
-
-  back(){
-    var swiper = document.querySelector('.swiper-container')['swiper'];
-    swiper.slidePrev();
-  }
-
-  next(){
-    var swiper = document.querySelector('.swiper-container')['swiper'];
-    swiper.slideNext();
-  }
-
-  today(){
-    this.calendar.currentDate = new Date();
-  }
-
-  async onEventSelected(event){
-    let start = formatDate(event.startTime, 'medium', this.locale);
-    let end = formatDate(event.endTime, 'medium', this.locale);
-  }
-
-  onViewTitleChanged(title){
-    this.viewTitle = title;
-  }
-
-  onTimeSelected(ev){
-    let selected = new Date(ev.selectedTime);
-    this.event.startTime = selected.toISOString();
-    selected.setHours(selected.getHours() +1);
-    this.event.endTime = (selected.toISOString());
-  }
-
-  onCurrentDateChanged(){
-
-  }
-
-  reloadSource(){
-
-  }
+  
 
 
   
